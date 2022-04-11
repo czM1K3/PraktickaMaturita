78833921845c234f5615cdeb0003c1c71300fbde
czM1K3 <misamadera1@gmail.com>
2022-04-11 11:34:04 +0200

    Final

--- src/Cyphers/Grid/Decode.tsx
+++ src/Cyphers/Grid/Decode.tsx
@@ -1,15 +1,31 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useState } from "react";
+import { Methods } from "../../enums/methods";
+import { myFetch } from "../../utils/myFetch";
+
+type ApiResponse = {
+	grid: number[][];
+	encoded: string;
+	token: string;
+};
 
 const GridSimpleDecode: FC = () => {
+	const [fetching, setFetching] = useState<boolean>(false);
+	const [grid, setGrid] = useState<number[][] | null>(null);
+	const [raw, setRaw] = useState<string>("");
 
 	const fetch = async () => {
-		alert("test")
+		setFetching(true);
+		const data: ApiResponse = await myFetch("https://sifrovani.maturita.delta-www.cz/grid-simple/decode", Methods.Get);
+		setFetching(false);
+		setGrid(data.grid);
+		setRaw(data.encoded);
 	};
 
 	return (
 		<>
 			<h1>Mřížka dekódování - jednoduchá</h1>
-			<button onClick={fetch}>Získat data</button>
+			<button onClick={fetch} disabled={fetching}>Získat data</button>
+			<p>Text: {raw === "" ? "Nejsou data" : raw}</p>
 		</>
 	);
 };
